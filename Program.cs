22b7677e65dbe758de9209008e840f4b63690d57
Rolf Madsen <rolf_madsen@keysight.com>
2018-10-09 10:22:24 +0200

    Added initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_code_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
